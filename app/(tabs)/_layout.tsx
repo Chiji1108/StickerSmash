e7da88dff5139dc6288b956ffde2b4e9f6285637
chiji1108 <shingo.pad@gmail.com>
2025-05-30 00:26:56 +0900

    Add tabs layout with Home and About screens, and implement Not Found screen

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,39 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: false,
+        headerTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: "About",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="information-circle" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
